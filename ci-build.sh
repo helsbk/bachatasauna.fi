fb4a5ee6c4f63cc95b22edcecbdd96348a2d61dd
Biru Mohanathas <birunthan@mohanathas.com>
2025-04-03 09:22:01 +0700

    Switch to dart-sass

--- ci-build.sh
+++ ci-build.sh
@@ -5,6 +5,13 @@ set -o errexit -o pipefail
 ZOLA_VERSION=0.20.0
 wget --quiet "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 tar xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+
+DART_SASS_VERSION=1.86.2
+wget --quiet "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+tar xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
+
+./dart-sass/sass --quiet --no-source-map -I sass sass/bundle.scss:static/css/bundle.css
+
 ./zola build
 
 find ./public -name 'index.html' -mindepth 2 -type f \
