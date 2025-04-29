bc8ea48d382cb7ed2ff2e7d4a581475c6961cf5d
Biru Mohanathas <birunthan@mohanathas.com>
2025-04-29 10:27:37 +0700

    Fix minify command on CI

--- ci-build.sh
+++ ci-build.sh
@@ -4,7 +4,7 @@ set -o errexit -o pipefail
 
 ZOLA_VERSION=0.20.0
 wget --quiet "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
-tar xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+tar xf "zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" 'zola'
 
 DART_SASS_VERSION=1.86.2
 wget --quiet "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
@@ -12,7 +12,7 @@ tar xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
 
 MINIFY_VERSION=2.23.2
 wget --quiet "https://github.com/tdewolff/minify/releases/download/v${MINIFY_VERSION}/minify_linux_amd64.tar.gz"
-tar xf "minify_linux_amd64.tar.gz"
+tar xf "minify_linux_amd64.tar.gz" 'minify'
 
 ./dart-sass/sass --quiet --no-source-map -I sass sass/bundle.scss:static/css/bundle.css
 
@@ -25,4 +25,4 @@ find ./public -name 'index.html' -mindepth 2 -type f \
 
 find ./public -empty -type d -delete
 
-./minify_linux_amd64/minify -r -i ./public
+./minify -r -i ./public
